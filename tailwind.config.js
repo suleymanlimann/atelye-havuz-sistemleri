efe53eb5cb37d66b693be1a22b37675094feff8f
suleymanlimann <suleymanliman28@gmail.com>
2025-08-10 23:03:41 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"]
+
+}
